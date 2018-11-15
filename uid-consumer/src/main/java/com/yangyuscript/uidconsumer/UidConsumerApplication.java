437f5bf5601e7d1bfb64495d1a64a36b47db109d
yangyuscript <1837475870@qq.com>
2018-11-15 17:16:55 +0800

    优化uid-generator集成配置，新集成spring-boot-admin，监控应用生命情况

--- uid-consumer/src/main/java/com/yangyuscript/uidconsumer/UidConsumerApplication.java
+++ uid-consumer/src/main/java/com/yangyuscript/uidconsumer/UidConsumerApplication.java
@@ -1,9 +1,13 @@
 package com.yangyuscript.uidconsumer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.baidu.fsg.uid.worker.dao")
 public class UidConsumerApplication {
 
     public static void main(String[] args) {
